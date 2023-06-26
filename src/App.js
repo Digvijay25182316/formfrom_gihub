2d67c6f4a6510f8fc518cc58140c312ebbb1427b
Digvijay Edake <digvijays.edake>
2023-06-26 13:05:30 +0530

    context directory removed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React from "react";
 import "./App.css";
-import { DataProvider } from "./context/DataContext";
 import MainPage from "./MainPage";
 
 function App() {
   return (
-    <DataProvider>
-      <div className="App">
-        <MainPage />
-        <div>
-          <p
-            style={{
-              textAlign: "center",
-              fontFamily: "Share Tech Mono, sans",
-              marginTop: "15px",
-              fontSize: "14px",
-              color: "#a50d0d",
-            }}
-          >
-            Designed And Coded By
-          </p>
-          <p
-            style={{
-              textAlign: "center",
-              color: "#00264d",
-              fontSize: "16px",
-              lineHeight: "26px",
-            }}
-          >
-            Digvijay Edake
-          </p>
-        </div>
+    <div className="App">
+      <MainPage />
+      <div>
+        <p
+          style={{
+            textAlign: "center",
+            fontFamily: "Share Tech Mono, sans",
+            marginTop: "15px",
+            fontSize: "14px",
+            color: "#a50d0d",
+          }}
+        >
+          Designed And Coded By
+        </p>
+        <p
+          style={{
+            textAlign: "center",
+            color: "#00264d",
+            fontSize: "16px",
+            lineHeight: "26px",
+          }}
+        >
+          Digvijay Edake
+        </p>
       </div>
-    </DataProvider>
+    </div>
   );
 }
 
