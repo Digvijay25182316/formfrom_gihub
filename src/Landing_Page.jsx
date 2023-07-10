11317fcaef1e1c32005051269eb589022eaaf41b
Digvijay Edake <digvijays.edake>
2023-07-10 09:22:31 +0530

    third update

--- src/Landing_Page.jsx
+++ src/Landing_Page.jsx
@@ -35,31 +35,31 @@ function Landingpage() {
             </div>
         </div>
         </div>
-        <div style={{marginTop:"50px ",display:"flex",flexDirection:"column",alignItems:"center"}} className='timing'>
+        <div style={{marginTop:"20px ",display:"flex",flexDirection:"column",alignItems:"center"}} className='timing'>
           <b>16 July 2023 (Sunday)</b>
           <b>Timing 6 AM to 5 PM</b>
         </div>
         <div style={{alignItems:"left",padding:"50px"}}>
           <p style={{textAlign:"center",fontSize:"34px",margin:"40px"}}>Schedule</p>
-      <p className='Schedule'>6:00 AM - Arrange at Katraj Bus Stop</p>
-      <p className='Schedule'>6:10 AM to 8 AM - Travel Time to ISCKON Rajgad</p>
-      <p className='Schedule'>8 AM - Darshan of Deities</p>
-      <p className='Schedule'>8:30 AM - Breakfast Prasadam</p>
-      <p className='Schedule'>9:00 AM to 11:00 AM - Session #1</p>
-      <p className='Schedule'>11:00 to 12:00 PM - Activity Time</p>
-      <p className='Schedule'>12:00 PM - Lunch Prasadam</p>
-      <p className='Schedule'>12:30 PM to 2 PM - Session #2</p>
-      <p className='Schedule'>2 PM to 5:00 PM - Activity Time</p>
-      <p className='Schedule'>5:00 PM - Leave Back for Katraj</p>
+      <p className='Schedule'>1.6:00 AM - Arrange at Katraj Bus Stop</p>
+      <p className='Schedule'>2.6:10 AM to 8 AM - Travel Time to ISCKON Rajgad</p>
+      <p className='Schedule'>3.8 AM - Darshan of Deities</p>
+      <p className='Schedule'>4.8:30 AM - Breakfast Prasadam</p>
+      <p className='Schedule'>5.9:00 AM to 11:00 AM - Session #1</p>
+      <p className='Schedule'>6.11:00 to 12:00 PM - Activity Time</p>
+      <p className='Schedule'>7.12:00 PM - Lunch Prasadam</p>
+      <p className='Schedule'>8.12:30 PM to 2 PM - Session #2</p>
+      <p className='Schedule'>9.2 PM to 5:00 PM - Activity Time</p>
+      <p className='Schedule'>10.5:00 PM - Leave Back for Katraj</p>
         </div>
         <div style={{alignItems:"left",margin:"23px"}}>
           <p style={{textAlign:"center",fontSize:"34px",marginBottom:"50px"}}>Thigs to carry</p>
-      <p className='Schedule'>Please Carry Extra Cloths,</p>
-      <p className='Schedule'>Towel & Umbrella or Raincoat</p>
-      <p className='Schedule'>.Carry your Water Bottle for a Trip</p>
-      <p className='Schedule'>Bring your Sweater or Shawl</p>
-      <p className='Schedule'>Dry snacks for travel,prasadam is arrangement</p>
-      <p className='Schedule'>Please Carry your Personal Medications (if any).</p>
+      <p className='Schedule'>1.Please Carry Extra Cloths,</p>
+      <p className='Schedule'>2.Towel & Umbrella or Raincoat</p>
+      <p className='Schedule'>3.Carry your Water Bottle for a Trip</p>
+      <p className='Schedule'>4.Bring your Sweater or Shawl</p>
+      <p className='Schedule'>5.Dry snacks for travel,prasadam is arrangement</p>
+      <p className='Schedule'>6.Please Carry your Personal Medications (if any).</p>
         </div>
     </div>
   )
