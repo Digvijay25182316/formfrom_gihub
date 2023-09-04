194eee734d9ee293cc98abaae0add900b37f9b25
Digvijay25182316 <digvijays.edake2002@gmail.com>
2023-09-04 18:15:07 +0530

    age field added

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,19 +3,20 @@ import { db, collection, addDoc } from "./firebase";
 import { useNavigate } from "react-router-dom";
 
 const Form = () => {
-  const navigate =useNavigate()
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     Fullname: "",
     PhoneNumber: "",
     city: "",
-    language:"",
+    language: "",
     gender: "",
-    program:[],
+    age: "",
+    program: [],
   });
   const handleProgramCheckboxChange = (e) => {
     const selectedProgram = e.target.value;
     const isChecked = e.target.checked;
-  
+
     // If the current checkbox is checked, uncheck all other checkboxes
     if (isChecked) {
       const updatedProgram = [selectedProgram];
@@ -48,60 +49,131 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(formData){
-    storedata(formData).then(data=>{
-      window.alert("successfully submitted")
-      setFormData({
-        Fullname: "",
-        PhoneNumber: "",
-        city: "",
-        language:"",
-        gender: "",
-        program:[],
-      })
-      navigate("/")
-    }).catch(err=>window.alert("Resubmit the form"))}
-    
+    if (formData) {
+      storedata(formData)
+        .then((data) => {
+          window.alert("successfully submitted");
+          setFormData({
+            Fullname: "",
+            PhoneNumber: "",
+            city: "",
+            language: "",
+            gender: "",
+            program: [],
+          });
+          navigate("/");
+        })
+        .catch((err) => window.alert("Resubmit the form"));
+    }
   };
   return (
     <div className="min-h-screen">
       <div className="">
-        <h1 className="text-4xl text-center my-10 font-extrabold">Register for Gitasaar</h1>
+        <h1 className="text-4xl text-center my-10 font-extrabold">
+          Register for Gitasaar
+        </h1>
         <div className="w-full flex flex-col items-center">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-8 mx-auto m-5">
-            <div className="md:w-[40vw] w-80 mx-auto  flex flex-col gap-3">
-              <label className="text-xl font-bold" htmlFor="name">Enter your full name <i className="text-bold text-red-500">*</i></label>              
-              <input onChange={handleChange} name="Fullname" type="text" className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600" required/>
-            </div>
-            <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
-              <label className="text-xl font-bold" htmlFor="phone number">Enter your Phone number <i className="text-bold text-red-500">*</i></label>              
-              <input onChange={handleChange} name="PhoneNumber" type="number" className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600" required maxLength={10}/>
-            </div>
-            <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
-              <label className="text-xl font-bold" htmlFor="language">Your preferred language <i className="text-bold text-red-500">*</i></label>              
-              <select name="language" type="text" className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600" required onChange={handleChange}>
-                <option value="">Your preferred language</option>
-                <option value="english">English</option>
-                <option value="hindi">Hindi</option>
-                <option value="marathi">Marathi</option>
-              </select>
-            </div>
-            <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
-              <label className="text-xl font-bold" htmlFor="Gender">Please choose Gender <i className="text-bold text-red-500">*</i></label>              
-              <select name="gender" type="text" className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600" required onChange={handleChange}>
-                <option value="">Enter your gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-            </div>
-            <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
-              <label className="text-xl font-bold" htmlFor="City">Location (enter the name of your city) <i className="text-bold text-red-500">*</i></label>              
-              <input onChange={handleChange} name="city" type="text" className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600" required/>
-            </div>
-            <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
-              <div className="text-xl font-bold" htmlFor="program_name">Which program you want to enrol in</div>
-              <div className="flex flex-col gap-3 text-xl font-bold">
+              <div className="md:w-[40vw] w-80 mx-auto  flex flex-col gap-3">
+                <label className="text-xl font-bold" htmlFor="name">
+                  Enter your full name{" "}
+                  <i className="text-bold text-red-500">*</i>
+                </label>
+                <input
+                  onChange={handleChange}
+                  name="Fullname"
+                  type="text"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                />
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <label className="text-xl font-bold" htmlFor="phone number">
+                  Enter your Phone number{" "}
+                  <i className="text-bold text-red-500">*</i>
+                </label>
+                <input
+                  onChange={handleChange}
+                  name="PhoneNumber"
+                  type="number"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                  maxLength={10}
+                />
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <label className="text-xl font-bold" htmlFor="language">
+                  Your preferred language{" "}
+                  <i className="text-bold text-red-500">*</i>
+                </label>
+                <select
+                  name="language"
+                  type="text"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                  onChange={handleChange}
+                >
+                  <option value="">Your preferred language</option>
+                  <option value="english">English</option>
+                  <option value="hindi">Hindi</option>
+                  <option value="marathi">Marathi</option>
+                </select>
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <label className="text-xl font-bold" htmlFor="Gender">
+                  Please choose Gender{" "}
+                  <i className="text-bold text-red-500">*</i>
+                </label>
+                <select
+                  name="gender"
+                  type="text"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                  onChange={handleChange}
+                >
+                  <option value="">Enter your gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <label className="text-xl font-bold" htmlFor="Gender">
+                  Choose Your age <i className="text-bold text-red-500">*</i>
+                </label>
+                <select
+                  name="age"
+                  type="text"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                  onChange={handleChange}
+                >
+                  <option value="">Select your age</option>
+                  <option value="15-20">15-20</option>
+                  <option value="20-30">20-30</option>
+                  <option value="30-40">30-40</option>
+                  <option value="40-50">40-50</option>
+                  <option value="50+">50+</option>
+                </select>
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <label className="text-xl font-bold" htmlFor="City">
+                  Location (enter the name of your city){" "}
+                  <i className="text-bold text-red-500">*</i>
+                </label>
+                <input
+                  onChange={handleChange}
+                  name="city"
+                  type="text"
+                  className="border shadow-sm hover:shadow-md drop-shadow-sm px-5 py-2 rounded-xl focus:outline-yellow-600"
+                  required
+                />
+              </div>
+              <div className="md:w-[40vw]  w-80 mx-auto flex flex-col gap-3 ">
+                <div className="text-xl font-bold" htmlFor="program_name">
+                  Which program you want to enrol in
+                </div>
+                <div className="flex flex-col gap-3 text-xl font-bold">
                   <label>
                     <input
                       type="checkbox"
@@ -110,7 +182,11 @@ const Form = () => {
                       value="Gitasar Temple program (Summary Study of Bhagvada Gita)"
                       onChange={handleProgramCheckboxChange}
                       className="w-5"
-                      disabled={formData.program.length>0&&formData.program[0]!=="Gitasar Temple program (Summary Study of Bhagvada Gita)"}
+                      disabled={
+                        formData.program.length > 0 &&
+                        formData.program[0] !==
+                          "Gitasar Temple program (Summary Study of Bhagvada Gita)"
+                      }
                     />
                     Gitasar Temple program (Summary Study of Bhagvada Gita)
                   </label>
@@ -124,13 +200,16 @@ const Form = () => {
                       value="CHILDREN Value Education"
                       onChange={handleProgramCheckboxChange}
                       className="w-5"
-                      disabled={formData.program.length>0&&formData.program[0]!=="CHILDREN Value Education"}
+                      disabled={
+                        formData.program.length > 0 &&
+                        formData.program[0] !== "CHILDREN Value Education"
+                      }
                       // checked={formData.program==="Gitasaar For Girls Program"}
                     />
                     CHILDREN Value Education
                   </label>
-              </div>
-              <div className="flex flex-col gap-3 text-xl font-bold">
+                </div>
+                <div className="flex flex-col gap-3 text-xl font-bold">
                   <label>
                     <input
                       type="checkbox"
@@ -139,12 +218,15 @@ const Form = () => {
                       value="Host program at your Society "
                       onChange={handleProgramCheckboxChange}
                       className="w-5"
-                      disabled={formData.program.length>0&&formData.program[0]!=="Host program at your Society "}
+                      disabled={
+                        formData.program.length > 0 &&
+                        formData.program[0] !== "Host program at your Society "
+                      }
                     />
-                    Host program at your Society 
+                    Host program at your Society
                   </label>
-              </div>
-              <div className="flex flex-col gap-3 text-xl font-bold">
+                </div>
+                <div className="flex flex-col gap-3 text-xl font-bold">
                   <label>
                     <input
                       type="checkbox"
@@ -153,13 +235,22 @@ const Form = () => {
                       value=" Daily Book Reading online (SPORT)"
                       onChange={handleProgramCheckboxChange}
                       className="w-5"
-                      disabled={formData.program.length>0&&formData.program[0]!==" Daily Book Reading online (SPORT)"}
+                      disabled={
+                        formData.program.length > 0 &&
+                        formData.program[0] !==
+                          " Daily Book Reading online (SPORT)"
+                      }
                     />
-                     Daily Book Reading online (SPORT)
+                    Daily Book Reading online (SPORT)
                   </label>
+                </div>
               </div>
-            </div>
-            <button type="submit" className="text-xl font-bold bg-gray-700 text-white px-3 py-1 rounded-xl hover:bg-gray-900 w-max">Confirm Registeration For Gita Saar</button>
+              <button
+                type="submit"
+                className="text-xl font-bold bg-gray-700 text-white px-3 py-1 rounded-xl hover:bg-gray-900 w-max"
+              >
+                Confirm Registeration For Gita Saar
+              </button>
             </div>
           </form>
         </div>
